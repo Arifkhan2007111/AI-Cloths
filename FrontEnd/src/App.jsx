863d0b555ce9d47a117e878ec5c3223876df6225
Arifkhan2007111 <arifkhan2007111@gmail.com>
2025-03-07 22:54:43 +0530

    Some changes

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -3,12 +3,13 @@ import './App.css'
 
 import HomePage from './components/UI'
 import ClothSelector from './components/UI2'
+import OutfitGenerator from './components/UI3'
 
 function App() {
 
   return (
     <>
-      <HomePage />
+      <OutfitGenerator />
     </>
   )
 }
